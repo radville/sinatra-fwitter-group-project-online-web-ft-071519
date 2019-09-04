89b34d5c8b675d4043a0a8af57a8d4ddb14e26aa
radville <laura.radville@gmail.com>
2019-09-04 10:49:37 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,42 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    if session[:user_id].nil?
+      erb :'users/signup'
+    else
+      redirect to '/tweets'
+    end
+  end
+
+  post '/signup' do
+    @user = User.new(:username => params[:username], :password => params[:password], :email => params[:email])
+    if @user.save && params[:username].length > 0 && params[:email].length > 0
+        session[:user_id] = @user.id
+        redirect to "/tweets"
+    else
+        redirect to "/signup"
+    end
+    binding.pry
+
+end
+
+  get '/login' do
+    erb :'users/login'
+  end
+
+  post '/login' do
+    @user = User.find_by(:username => params[:username], :password => params[:password], :email => params[:email])
+    session[:user_id] = @user.id
+  end
+
+  get '/logout' do
+    session.clear
+  end
+
+
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,15 @@
 class TweetsController < ApplicationController
 
+    get '/tweets' do
+        @tweets = Tweet.all
+        erb :'tweets/index'
+    end
+
+    post '/tweets' do
+        binding.pry
+        user = session[:user]
+        tweet = Tweet.create(content: params[:content], user_id: user.id)
+        erb :'tweets/index'
+    end
 
 end
